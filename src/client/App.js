15c5d5eef4b9631ea5766465a0ac59b538295ceb
Balachanderrao <balachander@betalectic.com>
2020-12-16 13:43:21 +0530

    initial commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Details from './IndiaPage/details.js'
-import DetailedOverView from './WorldPage/detailedOverView.js'
-import About from './common/about';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from "./IndiaPage/details.js";
+import DetailedOverView from "./WorldPage/detailedOverView.js";
+import About from "./common/about";
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Details} />
+          <Route exact path="/covid-19-react" component={Details} />
           <Route path="/world" component={DetailedOverView} />
           <Route path="/india" component={Details} />
           <Route path="/about" component={About} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 export default App;
